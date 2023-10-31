0c60f5fe6c38dd08908381a006b7887f706400ca
Husar Andrei <121796159+Dumitrozaur@users.noreply.github.com>
2023-10-31 14:39:36 +0200

    Log in facut

--- src/main/java/src/pricipal/proiect_final/PetShop.java
+++ src/main/java/src/pricipal/proiect_final/PetShop.java
@@ -1,12 +1,32 @@
 package src.pricipal.proiect_final;
 
+import java.util.ArrayList;
+
+import java.util.*;
+
 import javax.swing.JOptionPane;
 
+import org.bson.Document;
+
+import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+
 public class PetShop {
+	
+	static MongoClient client = MongoClients.create("mongodb+srv://Dumi:0ok9ij8uh@cluster0.ytjeywp.mongodb.net/?retryWrites=true&w=majority");
+	
+	static MongoDatabase db = client.getDatabase("PetShop");
+	
+	static MongoCollection<Document> col = db.getCollection("conturi");
+	
+	List<Document> dateBD = new ArrayList<>();
+	
+	
+    private static String usernameCorect;
+    private static String parolaCorecta;
 
-    private static String usernameCorect = "rares";
-    private static String parolaCorecta = "123456";
-
+    
+    
     public static void main(String[] args) {
 
         Meniu meniulMeu = new Meniu();
@@ -15,7 +35,12 @@ public class PetShop {
             int optiune = afisareMeniu();
 
             if (optiune == 1) {
-                if (signIn(usernameCorect, parolaCorecta)) {
+            	String enteredUsername = JOptionPane.showInputDialog("Introduceți username:");
+                String enteredPassword = JOptionPane.showInputDialog("Introduceți parola:");
+                
+                date = dataFetch();
+                
+                if (isValidUser(enteredUsername, enteredPassword, date)) {
                     JOptionPane.showMessageDialog(null, "Autentificare reușită! Programul poate continua.");
                     break;
                 } else {
@@ -27,24 +52,95 @@ public class PetShop {
                 JOptionPane.showMessageDialog(null, "Opțiune invalidă. Încercați din nou.");
             }
         }
+}
+    
+    public static List<Document> dataFetch(){
+        List<Document> dateBD = new ArrayList<>();
+        MongoCursor<Document> cursor = col.find().iterator();
+        while (cursor.hasNext()) {
+            dateBD.add(cursor.next());
+        }
+        return dateBD;
+    }
+    
+    static List<Document> date = dataFetch();
+    
+    private static boolean isValidUser(String enteredUsername, String enteredPassword, List<Document> date) {
+        if (enteredUsername != null && enteredPassword != null) {
+            for (Document doc : date) {
+                String usedUsername = doc.getString("Username");
+                String usedPassword = doc.getString("Parola");
+                
+                if (enteredUsername.equals(usedUsername) && enteredPassword.equals(usedPassword)) {
+                    return true; // Authentication successful
+                }
+            }
+        }
+        return false; // Authentication failed
+    }
+    
+    static String dataFetchUsername() {
+        Document document = col.find(Filters.eq("fieldToMatch", "valueToMatch")).first();
+        if (document != null) {
+            String username = document.getString("Username");
+            return username;
+        }else {
+        	
+        	return null;
+        	
+        }
     }
 
+    // Function to fetch the Parola from the database
+    static String dataFetchPassword() {
+        Document document = col.find(Filters.eq("fieldToMatch", "valueToMatch")).first();
+        if (document != null) {
+            String password = document.getString("Parola");
+            return password;
+        }else {
+        	
+        	return null;
+        	
+        }
+    }
+    
     // Funcția de Log In
-    private static boolean signIn(String usernameCorect, String parolaCorecta) {
+    private static void signIn() {
         String username = JOptionPane.showInputDialog("Introduceți username:");
         String parola = JOptionPane.showInputDialog("Introduceți parola:");
-
-        return username != null && parola != null && username.equals(usernameCorect) && parola.equals(parolaCorecta);
+        
+        boolean validare = isValidUser(username, parola, date);
+        
+        if (validare) {
+            JOptionPane.showMessageDialog(null, "Logare reușită!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Logare eșuată. Username sau parolă incorecte.");
+        }
+//       return username != null && parola != null && username.equals(usernameCorect) && parola.equals(parolaCorecta);
+    }
+    public static void inserareCont(String username, String parola) {
+    	Document contNou = new Document();
+    	contNou.append("Username", username);
+    	contNou.append("Parola", parola);
+    	
+    	col.insertOne(contNou);
     }
 
-    // Funcția de Creare Cont Nou
+    //Rares-Funcția de Creare Cont Nou 
+    //Andrei-Am updatat functia CreateAccount pentru a stoca conturile in baza de date
     private static void createAccount() {
         String usernameNou = JOptionPane.showInputDialog("Introduceți un nou username:");
         String parolaNoua = JOptionPane.showInputDialog("Introduceți o nouă parolă:");
-
         if (usernameNou != null && parolaNoua != null) {
-            usernameCorect = usernameNou;
-            parolaCorecta = parolaNoua;
+        	Document docCont = new Document();
+        	
+        	docCont.append("Username", usernameNou);
+        	docCont.append("Parola", parolaNoua);
+        	
+        	col.insertOne(docCont);
+        	
+//            usernameCorect = usernameNou;
+//            parolaCorecta = parolaNoua;
             JOptionPane.showMessageDialog(null, "Cont creat cu succes!");
         } else {
             JOptionPane.showMessageDialog(null, "Crearea contului a eșuat. Asigurați-vă că ați introdus username și parolă valide.");
